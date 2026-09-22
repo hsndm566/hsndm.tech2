99f4c3b1472d5f737f6e9e3f9d35250b404b17f5
Hasan Adam <hasanadam506@gmail.com>
2026-09-22 23:05:08 +0300

    Build AutoApply SA V2 preview: client/src/main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,40 +1,4 @@
-import { createRoot } from "react-dom/client";
-import { lazy, Suspense } from "react";
-import { useLocation } from "wouter";
-import App from "./App";
-import { installErrorTelemetry } from "./lib/errorTelemetry";
-import "./index.css";
-
-installErrorTelemetry();
-
-function activateDeferredFonts() {
-  document.querySelectorAll<HTMLLinkElement>('link[data-deferred-font="true"]').forEach(link => {
-    link.media = "all";
-    link.removeAttribute("data-deferred-font");
-  });
-}
-
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", activateDeferredFonts, { once: true });
-} else {
-  activateDeferredFonts();
-}
-
-const DataClientProviders = lazy(() => import("./components/DataClientProviders").then(module => ({ default: module.DataClientProviders })));
-const publicHomepageRoutes = new Set(["/", "/ar"]);
-
-function ApplicationBootstrap() {
-  const [location] = useLocation();
-
-  if (publicHomepageRoutes.has(location)) return <App />;
-
-  return (
-    <Suspense fallback={null}>
-      <DataClientProviders><App /></DataClientProviders>
-    </Suspense>
-  );
-}
-
-createRoot(document.getElementById("root")!).render(
-  <ApplicationBootstrap />,
-);
+import {createRoot} from 'react-dom/client';
+import App from './App';
+import './v2/base.css';
+createRoot(document.getElementById('root')!).render(<App/>);
